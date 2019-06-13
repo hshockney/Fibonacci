4d7540ca37fff83c47a05c2d972d2f6d5d2c8452
hshockney <shockney@atlanticbb.net>
2019-06-13 08:48:22 -0400

    Added second test

--- test_fibonacci.rb
+++ test_fibonacci.rb
@@ -6,4 +6,8 @@ class TestFibonacci < MiniTest::Test
        fib_of_zero = Fibonacci.of(0)
        assert_equal 0, fib_of_zero
    end
+    def test_fibonacci_of_one_is_one
+       fib_of_one = Fibonacci.of(1)
+       assert_equal 1, fib_of_one
+    end
 end
