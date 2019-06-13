c107664dea3e099699aca31c15c25b429198c610
hshockney <shockney@atlanticbb.net>
2019-06-13 09:05:52 -0400

    Refactoring method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,11 +1,10 @@
 class Fibonacci
     def self.of(n)
-       if n == 0
-           0
-       elsif n <= 2
+        return 0 if n == 0
+        if n <= 2
            1
-       else
-           return of(n-1) + of(n-2)
-       end
+        else
+            return of(n-1) + of(n-2)
+        end
     end
 end
