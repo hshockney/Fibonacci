d3e7d8335b6d0e936dc9c38b898e0655c610f2a8
hshockney <shockney@atlanticbb.net>
2019-06-13 09:07:14 -0400

    Removed conditional for cleaner code

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,7 @@
 class Fibonacci
     def self.of(n)
         return 0 if n == 0
-        if n <= 2
-           1
-        else
-            return of(n-1) + of(n-2)
-        end
+        return 1 if n <= 2
+        return of(n-1) + of(n-2)
     end
 end
