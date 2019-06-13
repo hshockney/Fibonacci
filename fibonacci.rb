692b1179f2e818d206482a917437e8e571faea78
hshockney <shockney@atlanticbb.net>
2019-06-13 09:02:12 -0400

    Added to conditional to get tests to pass

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,8 +2,10 @@ class Fibonacci
     def self.of(n)
        if n == 0
            0
-       else
+       elsif n <= 2
            1
+       else
+           return of(n-1) + of(n-2)
        end
     end
 end
